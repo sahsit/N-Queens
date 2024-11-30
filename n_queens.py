befed1c34dc157ddd2f0a9f2f44a427eaaac0b73
sahsit <salarhaider03@gmail.com>
2024-11-30 13:00:52 -0500

    add simulated annealing to algorithm

--- n_queens.py
+++ n_queens.py
@@ -2,6 +2,7 @@ from csp import nQueensCSP
 from csp import print_board
 import random
 import time
+import math
 
 def select_conflicted_queen(csp):
     # randomly selects conflicted queen to move for min_conflicts function
@@ -15,30 +16,51 @@ def select_conflicted_queen(csp):
 def min_conflicts(csp, max_steps):
     current = csp
     
+    
     for i in range(max_steps):
+        T = len(csp.variables) * 1000 # starting temperature is 1000 (high)
+        cooling_rate = 1 - (i / max_steps) * 0.05 # temperature should be decreased by T*0.95 every step
+        
+        # first, check if curr state is the solution
         if current.is_valid_solution():
             print(f"Solution found in {i} steps")
             return current
 
-        
-        
-        # select a random conflicted queen
+        # select a random conflicted queen (returns the column of that conflicted queen)
         var = select_conflicted_queen(current)
+        # counts current number of conflicts to compare later
+        current_conflicts = csp.conflicts(var)
+                
+        # find better position to put the queen (position that minimizes conflicts)
+        new_value = find_better_position(current, var)
+        # store the original column that the queen was in
+        original_value = csp.variables[var]
         
-        if var is None:
-            print("No conflicted queens left, but solution is invalid!")
-            return None
+        # update the column of the queen to the new position we found with new_value
+        current.variables[var] = new_value
+        # count number of conflicts that we'd have if we moved the queen to the new pos
+        new_conflicts = csp.conflicts(var)
         
+        # calculate difference between new and old conflicts
+        delta = new_conflicts - current_conflicts
         
-        # store a better position for that queen in "value"
-        value = find_better_position(current, var)
-        # update its posiion in variables list
-        current.variables[var] = value
+        # if the delta is less than 0, meaning the new_conflicts < current_conflicts...
+        if delta <= 0:
+            # ... then we move the queen to the new column because there's less conflicts
+            current.update_conflicted_queens(var)
+        else:
+            # else, calculate whether we should move the queen or not using the SA formula (accepting worse solution with certain probability)
+            if random.random() < math.exp(-delta/T):
+                current.update_conflicted_queens(var)
+            else:
+                # if the new state had more conflicts AND it didn't choose it based on the SA formula, just stay where you are
+                current.variables[var] = original_value
         
-        current.update_conflicted_queens(var)
+        # lower the temperature
+        T *= cooling_rate
             
-        if i % 100 == 0:  # Print every 10 steps
-            print(f"Step {i}: {len(current.conflicted_queens)} queens still in conflict")
+        if i % 50 == 0:  # Print every 50 steps
+            print(f"Step {i}: {len(current.conflicted_queens)} queens still in conflict (T={T:.2f})")
         
     print("Restart due to no progress.")
     return min_conflicts(nQueensCSP(len(csp.variables)), max_steps)
@@ -48,7 +70,7 @@ def find_better_position(csp, var):
     n = len(csp.variables)
     min_conflicts = float('inf')
     best_positions = [] # list to store best candidates for positions  
-    randomness = 0.1
+    randomness = 0.2
     
     # loop through each row
     for row in range(n):
@@ -80,7 +102,7 @@ def find_better_position(csp, var):
     
 def main():
     start_time = time.time()
-    csp = nQueensCSP(30)
+    csp = nQueensCSP(50)
     
 
     print(csp.variables)
@@ -88,7 +110,7 @@ def main():
     print("testing select_queen: ", num)
     num_conflicts= csp.conflicts(num)
     print("number of conflicts at col",num,": ", num_conflicts)
-    solution = min_conflicts(csp, 100)
+    solution = min_conflicts(csp, 500)
     if solution:
         print(solution.variables)
     end_time = time.time()
